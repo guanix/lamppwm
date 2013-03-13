19fa2818f5ee4c3de45f58758e25ca61fc2fecfa
Guan Yang <guan@yang.dk>
2013-03-13 17:58:48 -0400

    Works!

--- sleepworks.c
+++ sleepworks.c
@@ -15,7 +15,20 @@ void setup(void);
 void loop(void);
 void blink(uint8_t times, uint8_t speed);
 
-volatile uint8_t pcint_count;
+#define MODE_OFF 0
+#define MODE_LOW 1
+#define MODE_HIGH 2
+#define MODE_SLOWBLINK 3
+#define MODE_FASTBLINK 4
+#define MODE_MAX 5
+
+volatile uint8_t mode;
+volatile uint16_t pcint_count;
+volatile uint16_t timer0_count = 0;
+volatile uint8_t blink_on = 0;
+
+uint8_t bright = 0;
+uint8_t dim = 200;
 
 int main(void)
 {
@@ -23,27 +36,8 @@ int main(void)
 	while (1) loop();
 }
 
-void blink(uint8_t times, uint8_t speed)
-{
-    uint8_t i, j;
-
-    for (i = 0; i < times; i++) {
-        PORTB |= _BV(LED_PIN);
-        for (j = 0; j < speed; j++) {
-            _delay_ms(10);
-        }
-        PORTB &= ~(_BV(LED_PIN));
-        for (j = 0; j < speed; j++) {
-            _delay_ms(10);
-        }
-    }
-
-    _delay_ms(1200);
-}
-
 void setup()
 {
-//    blink(3, 30);
 	cli();
     // Set LED pin to output, bring high (P-channel MOSFET)
     // Enable pullup on button
@@ -55,10 +49,27 @@ void setup()
     GIMSK = _BV(PCIE);
 	GIFR = _BV(PCIF);
 	sei();
-    _delay_ms(200);
-	
-	set_sleep_mode(SLEEP_MODE_IDLE);
-    blink(5, 30);
+
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+    // Timer0 for blinking
+    TCCR0B = _BV(CS02) | _BV(CS00);     // Timer0 prescaler /1024
+    TIMSK = _BV(TOIE0);
+    TCNT0 = 0;
+
+    // Timer1 for PWM
+
+    GTCCR = _BV(PWM1B) | _BV(COM1B0);   // Timer1 PWM mode, OCR1B only
+    TCCR1 = _BV(CS11);
+//    TCCR1 = _BV(CS13) | _BV(CS10);
+    OCR1C = 255;
+    OCR1B = 255;
+    PORTB &= ~(_BV(LED_PIN));
+    TCNT1 = 0;
+
+//    PRR |= _BV(PRTIM1);
+
+    mode = MODE_OFF;
 }
 
 void loop()
@@ -66,21 +77,68 @@ void loop()
 	sleep_enable();
 	sleep_cpu();
 	sleep_disable();
-	blink(2, 15);
-	return;
-    volatile uint8_t blinks = pcint_count;
-    if (pcint_count == 0) return;
-    blink(blinks, 10);
-    pcint_count = 0;
-//	PORTB = (PORTB & _BV(BUTTON_PIN)) << (BUTTON_PIN - LED_PIN);
-// 	if (!(PINB & _BV(BUTTON_PIN))) {
-// 		_delay_ms(100);
-// 		if (!(PINB & _BV(BUTTON_PIN))) {
-// 			blink(1,30);
-// 		}
-// 	}
+
+    cli();
+
+    if (!(PINB & _BV(BUTTON_PIN))) {
+        _delay_ms(100);
+        if (!(PINB & _BV(BUTTON_PIN))) {
+            if (++mode == MODE_MAX) {
+                mode = 0;
+            }
+
+            switch (mode) {
+                case MODE_OFF:
+                    OCR1B = 255;
+//                    PRR |= _BV(PRTIM1);
+//                    PORTB &= ~(_BV(LED_PIN));
+                    break;
+                case MODE_LOW:
+                    OCR1B = dim;
+                    set_sleep_mode(SLEEP_MODE_IDLE);
+//                    PRR &= ~(_BV(PRTIM1));
+//                    TCNT1 = 0;
+                    break;
+                case MODE_HIGH:
+                    OCR1B = bright;
+                    set_sleep_mode(SLEEP_MODE_IDLE);
+//                    PRR &= ~(_BV(PRTIM1));
+//                    TCNT1 = 0;
+                    break;
+                case MODE_SLOWBLINK:
+                case MODE_FASTBLINK:
+                    OCR1B = 255;
+                    blink_on = 0;
+                    TCNT0 = 0;
+                    timer0_count = 0;
+                    set_sleep_mode(SLEEP_MODE_IDLE);
+                    break;
+            }
+
+            // Wait until button is released
+            while (!(PINB & _BV(BUTTON_PIN)));
+        }
+    }
+
+    sei();
 }
 
 ISR(PCINT0_vect) {
     pcint_count++;
+}
+
+ISR(TIMER0_OVF_vect) {
+    timer0_count++;
+
+    if ((mode == MODE_SLOWBLINK && timer0_count == 3) || (mode == MODE_FASTBLINK && timer0_count == 1)) {
+        blink_on = 1 - blink_on;
+
+        if (blink_on) {
+            OCR1B = bright;
+        } else {
+            OCR1B = 255;
+        }
+
+        timer0_count = 0;
+    }
 }
